e8b1166a1ae36fa209c710468d15fa6fbfdb4466
srinidhih <srinidhih1995@gmail.com>
2021-08-16 13:04:15 +0200

    Altering the format string to make it platform independent

--- rattestation/attestation.c
+++ rattestation/attestation.c
@@ -126,7 +126,7 @@ attestation_verify_resp(Tpm2dToRemote *resp, RAttestationConfig *config, uint8_t
 	size_t pcr_string_len = config->halg * 2;
 
 	if (resp->n_pcr_values != config->n_pcr_values) {
-		ERROR("Number of configured PCR values (%lu) does not match responded PCR values (%lu)",
+		ERROR("Number of configured PCR values (%zu) does not match responded PCR values (%zu)",
 		      config->n_pcr_values, resp->n_pcr_values);
 		ret = false;
 		goto err;
